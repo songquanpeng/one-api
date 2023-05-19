c19ac576d946777c08b4a6780b4b17d5c53ddfd2
analogpvt <72215823+analogpvt@users.noreply.github.com>
2023-05-19 23:18:18 +0530

    Update turnstile-check.go

--- middleware/turnstile-check.go
+++ middleware/turnstile-check.go
@@ -26,7 +26,7 @@ func TurnstileCheck() gin.HandlerFunc {
 			if response == "" {
 				c.JSON(http.StatusOK, gin.H{
 					"success": false,
-					"message": "Turnstile token 为空",
+					"message": "Turnstile token is empty",
 				})
 				c.Abort()
 				return
@@ -60,7 +60,7 @@ func TurnstileCheck() gin.HandlerFunc {
 			if !res.Success {
 				c.JSON(http.StatusOK, gin.H{
 					"success": false,
-					"message": "Turnstile 校验失败，请刷新重试！",
+					"message": "Turnstile Verification failed, please refresh and try again.！",
 				})
 				c.Abort()
 				return
@@ -69,7 +69,7 @@ func TurnstileCheck() gin.HandlerFunc {
 			err = session.Save()
 			if err != nil {
 				c.JSON(http.StatusOK, gin.H{
-					"message": "无法保存会话信息，请重试",
+					"message": "Unable to save session information, please try again.",
 					"success": false,
 				})
 				return
