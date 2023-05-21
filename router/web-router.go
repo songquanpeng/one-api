92c88fa273a4b55cf5a172a7b512cb6a848bb75a
JustSong <songquanpeng@foxmail.com>
2023-05-22 00:44:27 +0800

    fix: remove no-store for index.html

--- router/web-router.go
+++ router/web-router.go
@@ -16,7 +16,7 @@ func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 	router.Use(middleware.Cache())
 	router.Use(static.Serve("/", common.EmbedFolder(buildFS, "web/build")))
 	router.NoRoute(func(c *gin.Context) {
-		c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
+		c.Header("Cache-Control", "no-cache")
 		c.Data(http.StatusOK, "text/html; charset=utf-8", indexPage)
 	})
 }
