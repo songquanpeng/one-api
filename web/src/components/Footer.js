d79289ccdd1f49bc2e66b756bdd968ffc6615a4e
JustSong <songquanpeng@foxmail.com>
2023-06-17 11:03:01 +0800

    fix: fix footer not updated asap

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container, Segment } from 'semantic-ui-react';
 import { getFooterHTML, getSystemName } from '../helpers';
 
 const Footer = () => {
   const systemName = getSystemName();
-  const footer = getFooterHTML();
+  const [footer, setFooter] = useState(getFooterHTML());
+  let remainCheckTimes = 5;
+
+  const loadFooter = () => {
+    let footer_html = localStorage.getItem('footer_html');
+    if (footer_html) {
+      setFooter(footer_html);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (remainCheckTimes <= 0) {
+        clearInterval(timer);
+        return;
+      }
+      remainCheckTimes--;
+      loadFooter();
+    }, 200);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <Segment vertical>
