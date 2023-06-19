33846ce4f6de308bbd9733495ddff1df0fd6481a
JustSong <songquanpeng@foxmail.com>
2023-06-19 15:00:22 +0800

    refactor: rename function

--- controller/relay.go
+++ controller/relay.go
@@ -108,7 +108,7 @@ func Relay(c *gin.Context) {
 	case RelayModeImagesGenerations:
 		err = relayImageHelper(c, relayMode)
 	default:
-		err = relayHelper(c, relayMode)
+		err = relayTextHelper(c, relayMode)
 	}
 	if err != nil {
 		if err.StatusCode == http.StatusTooManyRequests {
