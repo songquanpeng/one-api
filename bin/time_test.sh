f51c98243715fc5f6982f08ab89f3636fc919567
JustSong <songquanpeng@foxmail.com>
2023-06-22 19:36:54 +0800

    fix: update time_test.sh

--- bin/time_test.sh
+++ bin/time_test.sh
@@ -16,7 +16,7 @@ for ((i=1; i<=count; i++)); do
            https://"$domain"/v1/chat/completions \
            -H "Content-Type: application/json" \
            -H "Authorization: Bearer $key" \
-           -d '{"prompt": "hi!", "max_tokens": 1}')
+           -d '{"prompt": "hi!", "max_tokens": 1, "model": "gpt-3.5-turbo"}')
   echo "$result"
   total_time=$(bc <<< "$total_time + $result")
   times+=("$result")
