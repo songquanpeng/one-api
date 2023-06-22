28a8c5cc478d498e8164be2ed2eb4322c8da11dd
mrhaoji <mrhaoji@gmail.com>
2023-06-22 19:52:28 +0800

    Update time_test.sh to fix the params

--- bin/time_test.sh
+++ bin/time_test.sh
@@ -16,7 +16,7 @@ for ((i=1; i<=count; i++)); do
            https://"$domain"/v1/chat/completions \
            -H "Content-Type: application/json" \
            -H "Authorization: Bearer $key" \
-           -d '{"messages": [{"content": "echo hi", "role": "user"}], "model": "gpt-3.5-turbo-0613", "stream": false, "max_tokens": 1}')
+           -d '{"messages": [{"content": "echo hi", "role": "user"}], "model": "gpt-3.5-turbo", "stream": false, "max_tokens": 1}')
   echo "$result"
   total_time=$(bc <<< "$total_time + $result")
   times+=("$result")
