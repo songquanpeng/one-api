ae28677b27139d23fff920c2d898ebefa4118a14
JustSong <songquanpeng@foxmail.com>
2023-06-25 23:13:48 +0800

    chore: do not hardcode

--- model/cache.go
+++ model/cache.go
@@ -108,7 +108,7 @@ var channelSyncLock sync.RWMutex
 func InitChannelCache() {
 	newChannelId2channel := make(map[int]*Channel)
 	var channels []*Channel
-	DB.Where("status = ?", 1).Find(&channels)
+	DB.Where("status = ?", common.ChannelStatusEnabled).Find(&channels)
 	for _, channel := range channels {
 		newChannelId2channel[channel.Id] = channel
 	}
