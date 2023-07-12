379d03798cda6637f3e0f49db8ff5d764291b771
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-07-12 21:14:30 +0800

    fix: add user edit discord

--- web/src/pages/User/EditUser.js
+++ web/src/pages/User/EditUser.js
@@ -13,13 +13,14 @@ const EditUser = () => {
     display_name: '',
     password: '',
     github_id: '',
+    discord_id: '',
     wechat_id: '',
     email: '',
     quota: 0,
     group: 'default'
   });
   const [groupOptions, setGroupOptions] = useState([]);
-  const { username, display_name, password, github_id, wechat_id, email, quota, group } =
+  const { username, display_name, password, github_id, wechat_id, email, quota, discord_id } =
     inputs;
   const handleInputChange = (e, { name, value }) => {
     setInputs((inputs) => ({ ...inputs, [name]: value }));
@@ -151,6 +152,16 @@ const EditUser = () => {
               label='已绑定的 GitHub 账户'
               name='github_id'
               value={github_id}
+              autoComplete='new-password'
+              placeholder='此项只读，需要用户通过个人设置页面的相关绑定按钮进行绑定，不可直接修改'
+              readOnly
+            />
+          </Form.Field>
+          <Form.Field>
+            <Form.Input
+              label='已绑定的 Discord 账户'
+              name='discord_id'
+              value={discord_id}
               autoComplete='new-password'
               placeholder='此项只读，需要用户通过个人设置页面的相关绑定按钮进行绑定，不可直接修改'
               readOnly
