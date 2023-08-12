ca512f6a38809f62a060391e838e750bc224be50
igophper <34326532+igophper@users.noreply.github.com>
2023-08-12 21:55:18 +0800

    refactor: add set event stream headers func (#402)
    封装流式传输响应头的设置

--- controller/relay-baidu.go
+++ controller/relay-baidu.go
@@ -191,11 +191,7 @@ func baiduStreamHandler(c *gin.Context, resp *http.Response) (*OpenAIErrorWithSt
 		}
 		stopChan <- true
 	}()
-	c.Writer.Header().Set("Content-Type", "text/event-stream")
-	c.Writer.Header().Set("Cache-Control", "no-cache")
-	c.Writer.Header().Set("Connection", "keep-alive")
-	c.Writer.Header().Set("Transfer-Encoding", "chunked")
-	c.Writer.Header().Set("X-Accel-Buffering", "no")
+	setEventStreamHeaders(c)
 	c.Stream(func(w io.Writer) bool {
 		select {
 		case data := <-dataChan:
