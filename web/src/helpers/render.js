cf7a2843b3ede9f7cf13e48d953d38bf04810fb3
Jungley Yeh <yejing@live.cn>
2023-08-13 15:15:51 +0800

    feat: Allow users to view their associated user groups and roles in the frontend
    close: #265
    
    仅在 PC端 展示，移动端不变

--- web/src/helpers/render.js
+++ web/src/helpers/render.js
@@ -7,6 +7,19 @@ export function renderText(text, limit) {
   return text;
 }
 
+export function renderRole(role) {
+  switch (role) {
+    case 1:
+      return <Label>普通用户</Label>;
+    case 10:
+      return <Label color='yellow'>管理员</Label>;
+    case 100:
+      return <Label color='orange'>超级管理员</Label>;
+    default:
+      return <Label color='red'>未知身份</Label>;
+  }
+}
+
 export function renderGroup(group) {
   if (group === '') {
     return <Label>default</Label>;
