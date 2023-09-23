3e667e59db3fce5adbd7fb4da79c11c0fd320d4b
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-09-23 13:36:29 +0800

    fix: remove unnecessary warning
    This warning is considered as redundant and unrelated to MIT license

--- web/src/components/OtherSetting.js
+++ web/src/components/OtherSetting.js
@@ -165,7 +165,6 @@ const OtherSetting = () => {
             />
           </Form.Group>
           <Form.Button onClick={submitAbout}>保存关于</Form.Button>
-          <Message>移除 One API 的版权标识必须首先获得授权，项目维护需要花费大量精力，如果本项目对你有意义，请主动支持本项目。</Message>
           <Form.Group widths='equal'>
             <Form.Input
               label='页脚'
