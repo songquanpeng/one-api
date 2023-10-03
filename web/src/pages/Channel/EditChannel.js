d663de3e3a65040231cf525dd326829699acf6ae
JustSong <songquanpeng@foxmail.com>
2023-10-03 12:52:45 +0800

    chore: delete 360's 360GPT_S2_V9.4

--- web/src/pages/Channel/EditChannel.js
+++ web/src/pages/Channel/EditChannel.js
@@ -76,7 +76,7 @@ const EditChannel = () => {
           localModels = ['SparkDesk'];
           break;
         case 19:
-          localModels = ['360GPT_S2_V9', 'embedding-bert-512-v1', 'embedding_s1_v1', 'semantic_similarity_s1_v1', '360GPT_S2_V9.4'];
+          localModels = ['360GPT_S2_V9', 'embedding-bert-512-v1', 'embedding_s1_v1', 'semantic_similarity_s1_v1'];
           break;
       }
       setInputs((inputs) => ({ ...inputs, models: localModels }));
