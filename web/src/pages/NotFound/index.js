6562beebbea525f32c0263b3cd006931d1c4b389
JustSong <songquanpeng@foxmail.com>
2023-10-14 12:41:45 +0800

    chore: update 404 page style

--- web/src/pages/NotFound/index.js
+++ web/src/pages/NotFound/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Segment, Header } from 'semantic-ui-react';
+import { Message } from 'semantic-ui-react';
 
 const NotFound = () => (
   <>
-    <Header
-      block
-      as="h4"
-      content="404"
-      attached="top"
-      icon="info"
-      className="small-icon"
-    />
-    <Segment attached="bottom">
-      未找到所请求的页面
-    </Segment>
+    <Message negative>
+      <Message.Header>页面不存在</Message.Header>
+      <p>请检查你的浏览器地址是否正确</p>
+    </Message>
   </>
 );
 
