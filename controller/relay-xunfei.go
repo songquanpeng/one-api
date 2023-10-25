cb2a597547b262f859a209cc7bf540dbbaee53af
abc <guohl1990@126.com>
2023-10-25 10:08:38 +0800

    fixed(xunfei): 支持科大讯飞3.0的模型参数

--- controller/relay-xunfei.go
+++ controller/relay-xunfei.go
@@ -297,9 +297,13 @@ func getXunfeiAuthUrl(c *gin.Context, apiKey string, apiSecret string) (string, 
 		apiVersion = "v1.1"
 		common.SysLog("api_version not found, use default: " + apiVersion)
 	}
+	
 	domain := "general"
 	if apiVersion == "v2.1" {
 		domain = "generalv2"
+	}
+	if apiVersion == "v3.1" {
+		domain = "generalv3"
 	}
 	authUrl := buildXunfeiAuthUrl(fmt.Sprintf("wss://spark-api.xf-yun.com/%s/chat", apiVersion), apiKey, apiSecret)
 	return domain, authUrl
