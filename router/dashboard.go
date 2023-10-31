2ce719ccc8c0b43caf5682dffc826646fa3ce6d6
andie <zchangan@163.com>
2023-10-31 14:52:13 +0800

    dashboard api enables cors

--- router/dashboard.go
+++ router/dashboard.go
@@ -9,6 +9,7 @@ import (
 
 func SetDashboardRouter(router *gin.Engine) {
 	apiRouter := router.Group("/")
+	apiRouter.Use(middleware.CORS())
 	apiRouter.Use(gzip.Gzip(gzip.DefaultCompression))
 	apiRouter.Use(middleware.GlobalAPIRateLimit())
 	apiRouter.Use(middleware.TokenAuth())
