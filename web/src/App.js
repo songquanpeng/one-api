6c762414e9f58931f25cb58dc5497acd1805a64d
CaIon <1808837298@qq.com>
2023-11-06 03:14:43 +0800

    修复mj选项卡找不到页面bug

--- web/src/App.js
+++ web/src/App.js
@@ -24,6 +24,7 @@ import EditRedemption from './pages/Redemption/EditRedemption';
 import TopUp from './pages/TopUp';
 import Log from './pages/Log';
 import Chat from './pages/Chat';
+import Midjourney from "./pages/Midjourney";
 
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
@@ -267,6 +268,14 @@ function App() {
           </PrivateRoute>
         }
       />
+        <Route
+            path='/midjourney'
+            element={
+                <PrivateRoute>
+                    <Midjourney />
+                </PrivateRoute>
+            }
+        />
       <Route
         path='/about'
         element={
