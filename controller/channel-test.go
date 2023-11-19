54e5f8ecd296a6d6d0662f6a16792ab3aa2cb9ab
Buer <42402987+MartialBE@users.noreply.github.com>
2023-11-19 15:52:35 +0800

    feat: support cloudflare gateway for azure (#666)
    * 🐛 Fix cloudflare gateway request failure
    
    * 🐛 fix channel test url error

--- controller/channel-test.go
+++ controller/channel-test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 	"one-api/common"
@@ -13,6 +12,8 @@ import (
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func testChannel(channel *model.Channel, request ChatRequest) (err error, openaiErr *OpenAIError) {
@@ -43,14 +44,14 @@ func testChannel(channel *model.Channel, request ChatRequest) (err error, openai
 	}
 	requestURL := common.ChannelBaseURLs[channel.Type]
 	if channel.Type == common.ChannelTypeAzure {
-		requestURL = fmt.Sprintf("%s/openai/deployments/%s/chat/completions?api-version=2023-03-15-preview", channel.GetBaseURL(), request.Model)
+		requestURL = getFullRequestURL(channel.GetBaseURL(), fmt.Sprintf("/openai/deployments/%s/chat/completions?api-version=2023-03-15-preview", request.Model), channel.Type)
 	} else {
 		if baseURL := channel.GetBaseURL(); len(baseURL) > 0 {
 			requestURL = baseURL
 		}
+
 		requestURL = getFullRequestURL(requestURL, "/v1/chat/completions", channel.Type)
 	}
-
 	jsonData, err := json.Marshal(request)
 	if err != nil {
 		return err, nil
