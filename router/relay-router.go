2a70744dbf266f25170ae04ca4ae7fe007ef6018
JustSong <songquanpeng@foxmail.com>
2023-12-10 19:53:33 +0800

    feat: add panic recover middleware

--- router/relay-router.go
+++ router/relay-router.go
@@ -17,7 +17,7 @@ func SetRelayRouter(router *gin.Engine) {
 		modelsRouter.GET("/:model", controller.RetrieveModel)
 	}
 	relayV1Router := router.Group("/v1")
-	relayV1Router.Use(middleware.TokenAuth(), middleware.Distribute())
+	relayV1Router.Use(middleware.RelayPanicRecover(), middleware.TokenAuth(), middleware.Distribute())
 	{
 		relayV1Router.POST("/completions", controller.Relay)
 		relayV1Router.POST("/chat/completions", controller.Relay)
