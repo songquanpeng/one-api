b7fcb319da622a6de340b34e9255efd5fae6cf19
JustSong <songquanpeng@foxmail.com>
2023-12-20 22:50:50 +0800

    chore: check if SESSION_SECRET equals to random_string

--- common/init.go
+++ common/init.go
@@ -36,7 +36,11 @@ func init() {
 	}
 
 	if os.Getenv("SESSION_SECRET") != "" {
-		SessionSecret = os.Getenv("SESSION_SECRET")
+		if os.Getenv("SESSION_SECRET") == "random_string" {
+			SysError("SESSION_SECRET is set to an example value, please change it to a random string.")
+		} else {
+			SessionSecret = os.Getenv("SESSION_SECRET")
+		}
 	}
 	if os.Getenv("SQLITE_PATH") != "" {
 		SQLitePath = os.Getenv("SQLITE_PATH")
