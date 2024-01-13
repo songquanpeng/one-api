983e77cb73a098cf4c2bda37db78ba197e4c516d
Martial BE <me@xiao5.info>
2024-01-13 15:40:25 +0800

    🐛 fix: key cannot be activated after expiration

--- web/src/views/Token/component/TableRow.js
+++ web/src/views/Token/component/TableRow.js
@@ -192,7 +192,7 @@ export default function TokensTableRow({ item, manageToken, handleOpenModal, set
               id={`switch-${item.id}`}
               checked={statusSwitch === 1}
               onChange={handleStatus}
-              disabled={statusSwitch !== 1 && statusSwitch !== 2}
+              // disabled={statusSwitch !== 1 && statusSwitch !== 2}
             />
           </Tooltip>
         </TableCell>
