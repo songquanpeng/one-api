619321ab2e051d5bc407c48a5c8034e0c47903a4
carey036 <45777074+carey036@users.noreply.github.com>
2024-03-01 12:28:49 +0800

    Update build.sh

--- web/build.sh
+++ web/build.sh
@@ -5,7 +5,6 @@ pwd
 
 while IFS= read -r theme; do
     echo "Building theme: $theme"
-    rm -rf build/$theme
     cd "$theme"
     npm install
     DISABLE_ESLINT_PLUGIN='true' REACT_APP_VERSION=$version npm run build
