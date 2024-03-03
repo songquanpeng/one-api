2df877a352f3ae150b276602172fc1ebcac21446
JustSong <songquanpeng@foxmail.com>
2024-03-03 22:14:07 +0800

    feat: switch priority when retry (close #1048)

--- middleware/distributor.go
+++ middleware/distributor.go
@@ -68,7 +68,7 @@ func Distribute() func(c *gin.Context) {
 				}
 			}
 			requestModel = modelRequest.Model
-			channel, err = model.CacheGetRandomSatisfiedChannel(userGroup, modelRequest.Model)
+			channel, err = model.CacheGetRandomSatisfiedChannel(userGroup, modelRequest.Model, false)
 			if err != nil {
 				message := fmt.Sprintf("当前分组 %s 下对于模型 %s 无可用渠道", userGroup, modelRequest.Model)
 				if channel != nil {
