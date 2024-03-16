d25ed4c0097e40ce448a526b0acdc8d988146339
JustSong <songquanpeng@foxmail.com>
2024-03-16 15:55:31 +0800

    chore: update name

--- web/air/src/helpers/utils.js
+++ web/air/src/helpers/utils.js
@@ -23,7 +23,7 @@ export function isRoot() {
 
 export function getSystemName() {
   let system_name = localStorage.getItem('system_name');
-  if (!system_name) return 'New API';
+  if (!system_name) return 'One API';
   return system_name;
 }
 
