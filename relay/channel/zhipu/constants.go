02539e8cd6b94fcfadad6867241b5ebd62425683
yangfei <yangfei@xuyao.info>
2024-04-04 15:27:38 +0800

    增加对智谱embedding-2模型的支持

--- relay/channel/zhipu/constants.go
+++ relay/channel/zhipu/constants.go
@@ -2,5 +2,5 @@ package zhipu
 
 var ModelList = []string{
 	"chatglm_turbo", "chatglm_pro", "chatglm_std", "chatglm_lite",
-	"glm-4", "glm-4v", "glm-3-turbo",
+	"glm-4", "glm-4v", "glm-3-turbo", "embedding-2",
 }
