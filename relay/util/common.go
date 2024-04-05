0bb7db0b4450d9f22008103585bfdbf90064ecd2
JustSong <songquanpeng@foxmail.com>
2024-04-05 12:11:50 +0800

    fix: do not detect quota field in error message (close #1276)

--- relay/util/common.go
+++ relay/util/common.go
@@ -46,9 +46,9 @@ func ShouldDisableChannel(err *relaymodel.Error, statusCode int) bool {
 	} else if strings.HasPrefix(err.Message, "This organization has been disabled.") {
 		return true
 	}
-	if strings.Contains(err.Message, "quota") {
-		return true
-	}
+	//if strings.Contains(err.Message, "quota") {
+	//	return true
+	//}
 	if strings.Contains(err.Message, "credit") {
 		return true
 	}
