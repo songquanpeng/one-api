a10232f43a435f7105a9efc49af23525ad94e118
JustSong <songquanpeng@foxmail.com>
2024-04-13 11:39:31 +0800

    feat: add gpt-4-turbo support (close #1304)

--- relay/billing/ratio/model.go
+++ relay/billing/ratio/model.go
@@ -29,6 +29,8 @@ var ModelRatio = map[string]float64{
 	"gpt-4-1106-preview":      5,    // $0.01 / 1K tokens
 	"gpt-4-0125-preview":      5,    // $0.01 / 1K tokens
 	"gpt-4-turbo-preview":     5,    // $0.01 / 1K tokens
+	"gpt-4-turbo":             5,    // $0.01 / 1K tokens
+	"gpt-4-turbo-2024-04-09":  5,    // $0.01 / 1K tokens
 	"gpt-4-vision-preview":    5,    // $0.01 / 1K tokens
 	"gpt-3.5-turbo":           0.25, // $0.0005 / 1K tokens
 	"gpt-3.5-turbo-0301":      0.75,
@@ -256,7 +258,7 @@ func GetCompletionRatio(name string) float64 {
 		return 4.0 / 3.0
 	}
 	if strings.HasPrefix(name, "gpt-4") {
-		if strings.HasSuffix(name, "preview") {
+		if strings.HasPrefix(name, "gpt-4-turbo") {
 			return 3
 		}
 		return 2
