58a0b6f9bc7527c68ea9cc000da7229a48ff81b5
Martial BE <me@xiao5.info>
2024-04-25 13:28:30 +0800

    🔖 chore: update bedrock model maping (#169)

--- providers/bedrock/category/base.go
+++ providers/bedrock/category/base.go
@@ -34,6 +34,7 @@ func GetCategory(modelName string) (*Category, error) {
 
 func GetModelName(modelName string) string {
 	bedrockMap := map[string]string{
+		"claude-3-opus-20240229":   "anthropic.claude-3-opus-20240229-v1:0",
 		"claude-3-sonnet-20240229": "anthropic.claude-3-sonnet-20240229-v1:0",
 		"claude-3-haiku-20240307":  "anthropic.claude-3-haiku-20240307-v1:0",
 		"claude-2.1":               "anthropic.claude-v2:1",
