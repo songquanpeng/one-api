8a34f768517fceb03a5d4168b5f4034089ce9ad7
MartialBE <me@xiao5.info>
2024-05-15 04:19:01 +0800

    🐛 fix: fix web price accuracy

--- web/src/views/Pricing/component/util.js
+++ web/src/views/Pricing/component/util.js
@@ -7,5 +7,8 @@ export function ValueFormatter(value) {
   if (value == null) {
     return '';
   }
-  return `$${parseFloat(value * 0.002).toFixed(4)} / ￥${parseFloat(value * 0.014).toFixed(4)}`;
+  if (value === 0) {
+    return '免费';
+  }
+  return `$${parseFloat(value * 0.002).toFixed(6)} / ￥${parseFloat(value * 0.014).toFixed(6)}`;
 }
