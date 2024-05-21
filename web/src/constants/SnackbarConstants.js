9bb90df5c10e25edf18ba5f7da0f936ed8c143f9
MartialBE <me@xiao5.info>
2024-05-21 14:37:34 +0800

    🔖 chore: Optimize tips and display the name of the log channel.(#199)

--- web/src/constants/SnackbarConstants.js
+++ web/src/constants/SnackbarConstants.js
@@ -1,35 +1,56 @@
+import { closeSnackbar } from 'notistack';
+import { IconX } from '@tabler/icons-react';
+import { IconButton } from '@mui/material';
+const action = (snackbarId) => (
+  <>
+    <IconButton
+      onClick={() => {
+        closeSnackbar(snackbarId);
+      }}
+    >
+      <IconX stroke={1.5} size="1.25rem" />
+    </IconButton>
+  </>
+);
+
 export const snackbarConstants = {
   Common: {
     ERROR: {
       variant: 'error',
       autoHideDuration: 5000,
-      preventDuplicate: true
+      preventDuplicate: true,
+      action
     },
     WARNING: {
       variant: 'warning',
       autoHideDuration: 10000,
-      preventDuplicate: true
+      preventDuplicate: true,
+      action
     },
     SUCCESS: {
       variant: 'success',
       autoHideDuration: 1500,
-      preventDuplicate: true
+      preventDuplicate: true,
+      action
     },
     INFO: {
       variant: 'info',
       autoHideDuration: 3000,
-      preventDuplicate: true
+      preventDuplicate: true,
+      action
     },
     NOTICE: {
       variant: 'info',
       autoHideDuration: 20000,
-      preventDuplicate: true
+      preventDuplicate: true,
+      action
     },
     COPY: {
       variant: 'copy',
       persist: true,
       preventDuplicate: true,
-      allowDownload: true
+      allowDownload: true,
+      action
     }
   },
   Mobile: {
