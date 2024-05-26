ec9b6d103930d150f73eddf109b9ed2ea47f24f5
MartialBE <me@xiao5.info>
2024-05-26 17:09:26 +0800

    feat: load channel models from server

--- web/berry/src/utils/common.js
+++ web/berry/src/utils/common.js
@@ -193,3 +193,29 @@ export function removeTrailingSlash(url) {
     return url;
   }
 }
+
+let channelModels = undefined;
+export async function loadChannelModels() {
+  const res = await API.get('/api/models');
+  const { success, data } = res.data;
+  if (!success) {
+    return;
+  }
+  channelModels = data;
+  localStorage.setItem('channel_models', JSON.stringify(data));
+}
+
+export function getChannelModels(type) {
+  if (channelModels !== undefined && type in channelModels) {
+    return channelModels[type];
+  }
+  let models = localStorage.getItem('channel_models');
+  if (!models) {
+    return [];
+  }
+  channelModels = JSON.parse(models);
+  if (type in channelModels) {
+    return channelModels[type];
+  }
+  return [];
+}
