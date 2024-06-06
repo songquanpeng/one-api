a6f7a53b02455151a22230462d8df9b684d9c2a5
jinjianming <57084209+jinjianming@users.noreply.github.com>
2024-06-06 20:47:45 +0800

    修复git、微信等用户注册不会创建默认令牌问题
    修复git、微信等用户注册不会创建默认令牌问题

--- model/user.go
+++ model/user.go
@@ -6,6 +6,7 @@ import (
 	"github.com/songquanpeng/one-api/common"
 	"github.com/songquanpeng/one-api/common/blacklist"
 	"github.com/songquanpeng/one-api/common/config"
+	"github.com/songquanpeng/one-api/common/helper"
 	"github.com/songquanpeng/one-api/common/logger"
 	"github.com/songquanpeng/one-api/common/random"
 	"gorm.io/gorm"
@@ -139,6 +140,21 @@ func (user *User) Insert(inviterId int) error {
 			_ = IncreaseUserQuota(inviterId, config.QuotaForInviter)
 			RecordLog(inviterId, LogTypeSystem, fmt.Sprintf("邀请用户赠送 %s", common.LogQuota(config.QuotaForInviter)))
 		}
+	}
+	// 创建默认令牌
+	cleanToken := Token{
+		UserId:         user.Id,
+		Name:           "default",
+		Key:            random.GenerateKey(),
+		CreatedTime:    helper.GetTimestamp(),
+		AccessedTime:   helper.GetTimestamp(),
+		ExpiredTime:    -1,
+		RemainQuota:    -1,
+		UnlimitedQuota: true,
+	}
+	result.Error = cleanToken.Insert()
+	if result.Error != nil {
+		return result.Error
 	}
 	return nil
 }
