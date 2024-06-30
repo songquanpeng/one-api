34cb147a744e717404ebccd566cdf1b753ef78a1
igophper <34326532+igophper@users.noreply.github.com>
2024-06-30 16:13:43 +0800

    refactor: replace hardcoded string with ctxkey constant (#1579)
    Co-authored-by: 江杭辉 <jianghanghui@k.app>

--- controller/relay.go
+++ controller/relay.go
@@ -48,7 +48,7 @@ func Relay(c *gin.Context) {
 		logger.Debugf(ctx, "request body: %s", string(requestBody))
 	}
 	channelId := c.GetInt(ctxkey.ChannelId)
-	userId := c.GetInt("id")
+	userId := c.GetInt(ctxkey.Id)
 	bizErr := relayHelper(c, relayMode)
 	if bizErr == nil {
 		monitor.Emit(channelId, true)
