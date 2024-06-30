dcb4b3e4dc67111847625507e3c5096c6e864924
lihangfu <280001404@qq.com>
2024-06-30 19:37:07 +0800

    feat: support spark4.0 ultra (#1569)
    * feat: 支持v3最新协议的腾讯混元（#1452）
    
    * feat: 支持Spark4.0 Ultra
    
    ---------
    
    Co-authored-by: lihangfu <hfli8@iflytek.com>

--- web/air/src/pages/Channel/EditChannel.js
+++ web/air/src/pages/Channel/EditChannel.js
@@ -78,7 +78,7 @@ const EditChannel = (props) => {
                     localModels = ['chatglm_pro', 'chatglm_std', 'chatglm_lite'];
                     break;
                 case 18:
-                    localModels = ['SparkDesk', 'SparkDesk-v1.1', 'SparkDesk-v2.1', 'SparkDesk-v3.1', 'SparkDesk-v3.5'];
+                    localModels = ['SparkDesk', 'SparkDesk-v1.1', 'SparkDesk-v2.1', 'SparkDesk-v3.1', 'SparkDesk-v3.5', 'SparkDesk-v4.0'];
                     break;
                 case 19:
                     localModels = ['360GPT_S2_V9', 'embedding-bert-512-v1', 'embedding_s1_v1', 'semantic_similarity_s1_v1'];
