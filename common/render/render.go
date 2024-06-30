d05344c543d7ed2d69fff16a5a7cc35f43ee5235
JustSong <quanpengsong@gmail.com>
2024-06-30 18:30:46 +0800

    refactor: add back trim & flush

--- common/render/render.go
+++ common/render/render.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/songquanpeng/one-api/common"
+	"strings"
 )
 
 func StringData(c *gin.Context, str string) {
+	str = strings.TrimPrefix(str, "data: ")
+	str = strings.TrimSuffix(str, "\r")
 	c.Render(-1, common.CustomEvent{Data: "data: " + str})
+	c.Writer.Flush()
 }
 
 func ObjectData(c *gin.Context, object interface{}) error {
