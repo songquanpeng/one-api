adba54acd34784bbf33a55259e10ebab51aa4263
Laisky.Cai <github@laisky.com>
2024-07-16 23:48:54 +0800

    fix: implement improved headers for anthropic to support 8k outputs (#1654)

--- relay/adaptor/vertexai/registry.go
+++ relay/adaptor/vertexai/registry.go
@@ -32,7 +32,6 @@ func init() {
 	}
 }
 
-
 type innerAIAdapter interface {
 	ConvertRequest(c *gin.Context, relayMode int, request *model.GeneralOpenAIRequest) (any, error)
 	DoResponse(c *gin.Context, resp *http.Response, meta *meta.Meta) (usage *model.Usage, err *model.ErrorWithStatusCode)
--- relay/meta/relay_meta.go
+++ relay/meta/relay_meta.go
@@ -10,20 +10,22 @@ import (
 )
 
 type Meta struct {
-	Mode            int
-	ChannelType     int
-	ChannelId       int
-	TokenId         int
-	TokenName       string
-	UserId          int
-	Group           string
-	ModelMapping    map[string]string
-	BaseURL         string
-	APIKey          string
-	APIType         int
-	Config          model.ChannelConfig
-	IsStream        bool
+	Mode         int
+	ChannelType  int
+	ChannelId    int
+	TokenId      int
+	TokenName    string
+	UserId       int
+	Group        string
+	ModelMapping map[string]string
+	BaseURL      string
+	APIKey       string
+	APIType      int
+	Config       model.ChannelConfig
+	IsStream     bool
+	// OriginModelName is the model name from the raw user request
 	OriginModelName string
+	// ActualModelName is the model name after mapping
 	ActualModelName string
 	RequestURLPath  string
 	PromptTokens    int // only for DoResponse
