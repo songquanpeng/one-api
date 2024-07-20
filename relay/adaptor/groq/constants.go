39453a7aa11f60d429d8fd6320208a1e45adf826
longkeyy <longkeyy@gmail.com>
2024-07-20 20:56:34 +0800

    feat:synchronize with the official release of the groq model
    update groq add gemma2-9b-it llama3.1 family fixup price k/token -> m/token

--- relay/adaptor/groq/constants.go
+++ relay/adaptor/groq/constants.go
@@ -4,9 +4,14 @@ package groq
 
 var ModelList = []string{
 	"gemma-7b-it",
-	"llama2-7b-2048",
-	"llama2-70b-4096",
 	"mixtral-8x7b-32768",
 	"llama3-8b-8192",
 	"llama3-70b-8192",
+	"gemma2-9b-it",
+	"llama-3.1-405b-reasoning",
+	"llama-3.1-70b-versatile",
+	"llama-3.1-8b-instant",
+	"llama3-groq-70b-8192-tool-use-preview",
+	"llama3-groq-8b-8192-tool-use-preview",
+	"whisper-large-v3",
 }
