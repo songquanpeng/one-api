22acb539d1fa311c33df6967231293fd4698d60d
longkeyy <longkeyy@gmail.com>
2024-07-20 20:56:34 +0800

    feat: update gemini model and price

--- relay/adaptor/gemini/constants.go
+++ relay/adaptor/gemini/constants.go
@@ -3,6 +3,5 @@ package gemini
 // https://ai.google.dev/models/gemini
 
 var ModelList = []string{
-	"gemini-pro", "gemini-1.0-pro-001", "gemini-1.5-pro",
-	"gemini-pro-vision", "gemini-1.0-pro-vision-001", "embedding-001", "text-embedding-004",
+	"gemini-pro", "gemini-1.0-pro", "gemini-1.5-flash", "gemini-1.5-pro", "text-embedding-004", "aqa",
 }
