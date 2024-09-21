649ecbf29c6d8f53aa86ae34faf8272916bb939c
TAKO <20227709+HynoR@users.noreply.github.com>
2024-09-21 23:01:19 +0800

    feat: support new openai models (4o 0806, chatgpt-4o-latest) (#1721)
    * feat: support new model gpt-4o-2024-08-06
    
    * feat: support new model chatgpt-4o-latest

--- relay/billing/ratio/model.go
+++ relay/billing/ratio/model.go
@@ -34,7 +34,9 @@ var ModelRatio = map[string]float64{
 	"gpt-4-turbo":             5,     // $0.01 / 1K tokens
 	"gpt-4-turbo-2024-04-09":  5,     // $0.01 / 1K tokens
 	"gpt-4o":                  2.5,   // $0.005 / 1K tokens
+	"chatgpt-4o-latest":       2.5,   // $0.005 / 1K tokens
 	"gpt-4o-2024-05-13":       2.5,   // $0.005 / 1K tokens
+	"gpt-4o-2024-08-06":       1.25,  // $0.0025 / 1K tokens
 	"gpt-4o-mini":             0.075, // $0.00015 / 1K tokens
 	"gpt-4o-mini-2024-07-18":  0.075, // $0.00015 / 1K tokens
 	"gpt-4-vision-preview":    5,     // $0.01 / 1K tokens
@@ -320,7 +322,7 @@ func GetCompletionRatio(name string, channelType int) float64 {
 		return 4.0 / 3.0
 	}
 	if strings.HasPrefix(name, "gpt-4") {
-		if strings.HasPrefix(name, "gpt-4o-mini") {
+		if strings.HasPrefix(name, "gpt-4o-mini") || name == "gpt-4o-2024-08-06" {
 			return 4
 		}
 		if strings.HasPrefix(name, "gpt-4-turbo") ||
@@ -329,6 +331,9 @@ func GetCompletionRatio(name string, channelType int) float64 {
 			return 3
 		}
 		return 2
+	}
+	if name == "chatgpt-4o-latest" {
+		return 3
 	}
 	if strings.HasPrefix(name, "claude-3") {
 		return 5
