c6cdaccc4212b91a2d39636358764d8c413a84d3
JustSong <songquanpeng@foxmail.com>
2024-09-21 22:30:12 +0800

    chore: add missing space

--- monitor/manage.go
+++ monitor/manage.go
@@ -15,7 +15,7 @@ func ShouldDisableChannel(err *model.Error, statusCode int) bool {
 	if err == nil {
 		return false
 	}
-	if statusCode == http.StatusUnauthorized{
+	if statusCode == http.StatusUnauthorized {
 		return true
 	}
 	switch err.Type {
