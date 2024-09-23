1b3f621e7135df620510a59d9482450f4c645f4e
lihangfu <hfli8@iflytek.com>
2024-09-23 11:24:22 +0800

    feat: support SparkDesk-v3.5-32K

--- relay/billing/ratio/model.go
+++ relay/billing/ratio/model.go
@@ -130,6 +130,7 @@ var ModelRatio = map[string]float64{
 	"SparkDesk-v3.1":            1.2858, // ￥0.018 / 1k tokens
 	"SparkDesk-v3.1-128K":       1.2858, // ￥0.018 / 1k tokens
 	"SparkDesk-v3.5":            1.2858, // ￥0.018 / 1k tokens
+	"SparkDesk-v3.5-32K":        1.2858, // ￥0.018 / 1k tokens
 	"SparkDesk-v4.0":            1.2858, // ￥0.018 / 1k tokens
 	"360GPT_S2_V9":              0.8572, // ¥0.012 / 1k tokens
 	"embedding-bert-512-v1":     0.0715, // ¥0.001 / 1k tokens
