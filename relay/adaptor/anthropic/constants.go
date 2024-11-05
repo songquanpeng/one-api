2f57dada74eda080fe343a1018300b338c3046e5
Laisky.Cai <github@laisky.com>
2024-11-05 00:50:18 +0000

    feat: update model lists and mappings for Claude 3.5 versions

--- relay/adaptor/anthropic/constants.go
+++ relay/adaptor/anthropic/constants.go
@@ -7,4 +7,6 @@ var ModelList = []string{
 	"claude-3-opus-20240229",
 	"claude-3-5-sonnet-20240620",
 	"claude-3-5-sonnet-20241022",
+	"claude-3-5-sonnet-latest",
+	"claude-3-5-haiku-20241022",
 }
--- relay/adaptor/aws/claude/main.go
+++ relay/adaptor/aws/claude/main.go
@@ -29,11 +29,13 @@ var AwsModelIDMap = map[string]string{
 	"claude-instant-1.2":         "anthropic.claude-instant-v1",
 	"claude-2.0":                 "anthropic.claude-v2",
 	"claude-2.1":                 "anthropic.claude-v2:1",
+	"claude-3-haiku-20240307":    "anthropic.claude-3-haiku-20240307-v1:0",
 	"claude-3-sonnet-20240229":   "anthropic.claude-3-sonnet-20240229-v1:0",
+	"claude-3-opus-20240229":     "anthropic.claude-3-opus-20240229-v1:0",
 	"claude-3-5-sonnet-20240620": "anthropic.claude-3-5-sonnet-20240620-v1:0",
 	"claude-3-5-sonnet-20241022": "anthropic.claude-3-5-sonnet-20241022-v2:0",
-	"claude-3-opus-20240229":     "anthropic.claude-3-opus-20240229-v1:0",
-	"claude-3-haiku-20240307":    "anthropic.claude-3-haiku-20240307-v1:0",
+	"claude-3-5-sonnet-latest":   "anthropic.claude-3-5-sonnet-20241022-v2:0",
+	"claude-3-5-haiku-20241022":  "anthropic.claude-3-5-haiku-20241022-v1:0",
 }
 
 func awsModelID(requestModel string) (string, error) {
