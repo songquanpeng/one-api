cbfc983dc303bd93f09d1c7fa1f248c09dd97105
Laisky.Cai <github@laisky.com>
2024-11-09 22:48:54 +0800

    feat: add new claude models (#1910)
    * feat: Add new models to ModelList in constants.go
    
    * feat: update model lists and mappings for Claude 3.5 versions
    
    ---------
    
    Co-authored-by: JustSong <39998050+songquanpeng@users.noreply.github.com>

--- relay/adaptor/groq/constants.go
+++ relay/adaptor/groq/constants.go
@@ -11,7 +11,9 @@ var ModelList = []string{
 	"llama-3.2-11b-vision-preview",
 	"llama-3.2-1b-preview",
 	"llama-3.2-3b-preview",
+	"llama-3.2-11b-vision-preview",
 	"llama-3.2-90b-text-preview",
+	"llama-3.2-90b-vision-preview",
 	"llama-guard-3-8b",
 	"llama3-70b-8192",
 	"llama3-8b-8192",
@@ -21,4 +23,5 @@ var ModelList = []string{
 	"mixtral-8x7b-32768",
 	"distil-whisper-large-v3-en",
 	"whisper-large-v3",
+	"whisper-large-v3-turbo",
 }
--- relay/adaptor/vertexai/claude/adapter.go
+++ relay/adaptor/vertexai/claude/adapter.go
@@ -13,7 +13,12 @@ import (
 )
 
 var ModelList = []string{
-	"claude-3-haiku@20240307", "claude-3-opus@20240229", "claude-3-5-sonnet@20240620", "claude-3-sonnet@20240229",
+	"claude-3-haiku@20240307",
+	"claude-3-sonnet@20240229",
+	"claude-3-opus@20240229",
+	"claude-3-5-sonnet@20240620",
+	"claude-3-5-sonnet-v2@20241022",
+	"claude-3-5-haiku@20241022",
 }
 
 const anthropicVersion = "vertex-2023-10-16"
