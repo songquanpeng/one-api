45be89e141f73aeac4726d01f217c38677e10da7
CalciumIon <1808837298@qq.com>
2024-12-04 23:27:40 +0800

    feat: support gzip decode

--- router/relay.go
+++ router/relay.go
@@ -9,6 +9,7 @@ import (
 
 func SetRelayRouter(router *gin.Engine) {
 	router.Use(middleware.CORS())
+	router.Use(middleware.GzipDecodeMiddleware())
 	// https://platform.openai.com/docs/api-reference/introduction
 	modelsRouter := router.Group("/v1/models")
 	modelsRouter.Use(middleware.TokenAuth())
