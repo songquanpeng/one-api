93fdb60de5bb7ecc5ae1ca7e6b65cedb5880263c
JustSong <quanpengsong@gmail.com>
2025-02-01 00:21:04 +0800

    feat: update log table style

--- web/default/src/pages/Log/index.js
+++ web/default/src/pages/Log/index.js
@@ -6,7 +6,7 @@ const Log = () => (
   <div className='dashboard-container'>
     <Card fluid className='chart-card'>
       <Card.Content>
-        <Card.Header className='header'>操作日志</Card.Header>
+        {/*<Card.Header className='header'>操作日志</Card.Header>*/}
         <LogsTable />
       </Card.Content>
     </Card>
