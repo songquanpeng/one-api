c715f2bc1da22412e44d719bbdd4a7a380f7cc0c
JustSong <quanpengsong@gmail.com>
2025-02-09 21:21:28 +0800

    feat: add new models for xai

--- relay/adaptor/xai/constants.go
+++ relay/adaptor/xai/constants.go
@@ -1,5 +1,14 @@
 package xai
 
+//https://console.x.ai/
+
 var ModelList = []string{
+	"grok-2",
+	"grok-vision-beta",
+	"grok-2-vision-1212",
+	"grok-2-vision",
+	"grok-2-vision-latest",
+	"grok-2-1212",
+	"grok-2-latest",
 	"grok-beta",
 }
