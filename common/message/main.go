8f6b13c2fead1fca214a2016efb64ea416840ae7
MyronZhang <zhangzhishun1024@gmail.com>
2025-02-11 16:17:53 +0800

    fix send ByAll

--- common/message/main.go
+++ common/message/main.go
@@ -12,6 +12,16 @@ const (
 )
 
 func Notify(by string, title string, description string, content string) error {
+	if by == ByAll {
+		if err := SendEmail(title, config.RootUserEmail, content); err != nil {
+			return fmt.Errorf("failed to send email: %v", err)
+		}
+		if err := SendMessage(title, description, content); err != nil {
+			return fmt.Errorf("failed to send message: %v", err)
+		}
+		return nil
+	}
+
 	if by == ByEmail {
 		return SendEmail(title, config.RootUserEmail, content)
 	}
